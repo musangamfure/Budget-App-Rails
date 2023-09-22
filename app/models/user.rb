d48890a39ca01c8a5643c96eb27c8218f577ecd4
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-09-22 10:51:27 +0100

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :categories, class_name: 'Category', dependent: :destroy, foreign_key: 'user_id'
+  has_many :bills, class_name: 'Bill', dependent: :destroy, foreign_key: 'user_id'
+
+  validates :name, presence: true
 end
