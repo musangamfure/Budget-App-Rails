30e811462f681b890edee95b18071d539fdbf44d
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-09-22 10:50:42 +0100

    Update bill model

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,10 @@
 class Bill < ApplicationRecord
+  has_many :category_bills, dependent: :destroy
+  has_many :categories, through: :category_bills
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+
+  has_one_attached :icon
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { only_float: true, greater_than_or_equal_to: 0 }
 end
