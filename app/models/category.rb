8a5e097c0751a40d6fe4fdb5a02d74dcb93566fc
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-09-22 10:51:06 +0100

    Update category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  has_many :category_bills, dependent: :destroy
+  has_many :bills, through: :category_bills
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+
+  has_one_attached :icon
+
+  validates :name, presence: true
 end
