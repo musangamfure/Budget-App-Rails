ef4fb115ff5a13a0fbc346443cf21e2afba0dab9
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-09-27 15:29:58 +0100

    Update billcontroller and routes

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,17 +1,20 @@
 class BillsController < ApplicationController
-  before_action :find_category, only: [:new, :create]
+  before_action :find_category
   before_action :ensure_category_owner, only: :create
 
   def new
-    @bill = Bill.new
+    @bill = @category.bills.build
   end
 
   def create
+    @bill = @category.bills.build(bill_params)
+    @bill.user = current_user
+
     if @bill.save
       flash[:notice] = 'Bill created successfully'
-      redirect_to @bill.categories.first
+      redirect_to category_path(@category)
     else
-      flash[:alert] = @bill.errors.full_messages.first if @bill.errors.any?
+      flash.now[:alert] = @bill.errors.full_messages.first if @bill.errors.any?
       render :new, status: :unprocessable_entity
     end
   end
@@ -19,18 +22,16 @@ class BillsController < ApplicationController
   private
 
   def bill_params
-    params.require(:bill).permit(:name, :amount, category_ids: [])
+    params.require(:bill).permit(:name, :amount)
   end
 
   def find_category
     @category = Category.find(params[:category_id])
-    @bill = @category.bills.build(bill_params)
-    @bill.user = current_user
   end
 
   def ensure_category_owner
     unless @category.user == current_user
-      redirect_to categories_path, notice: 'You can only create bills from your categories'
+      redirect_to categories_path, notice: 'You can only create bills in your categories'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
- 
- 
+
   resources :landings, only: [:index]
-  resources :categories do
-    resources :bills
+
+  resources :categories, path: 'categories' do
+    resources :bills, path: 'bills', as: 'category_bills', only: [:new, :create]
   end
+
   root "landings#index"
 end
